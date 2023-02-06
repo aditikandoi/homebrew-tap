04201bc3a9816af01764294a267b32dd6f65d2c5
aditikandoi <54035031+aditikandoi@users.noreply.github.com>
2023-02-06 14:04:43 -0500

    Update mytool.rb

--- Formula/mytool.rb
+++ Formula/mytool.rb
@@ -1,6 +1,6 @@
 require "formula"
 require_relative "../custom_download_strategy.rb"
-class Wget < Formula
+class Mytool < Formula
   homepage "https://www.sigscalr.io/index.html#getting-started/"
   url "https://sigscalr-releases.s3.amazonaws.com/sigscalr-1.1.12-darwin-arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy 
   sha256 "52126be8cf1bddd7536886e74c053ad7d0ed2aa89b4b630f76785bac21695fcd"
